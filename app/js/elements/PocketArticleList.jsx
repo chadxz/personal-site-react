0649ee2bfe8d5058bcda2f041d26dc35654ccac6
Chad McElligott <chad.mcelligott@gmail.com>
2015-11-11 12:34:34 -0600

    Add pocket bg image and host to article title

--- app/js/elements/PocketArticleList.jsx
+++ app/js/elements/PocketArticleList.jsx
@@ -1,16 +1,19 @@
 import React, { PropTypes } from 'react';
+import url from 'url';
 import moment from 'moment';
 
 function PinboardBookmarkList({ articles }) {
   const dateTimeFormat = 'MMMM D, YYYY @ h:mm a';
   const articleNodes = articles.map(article => {
+    const articleHost = url.parse(article.resolved_url).host;
     const relativeDateTime = moment.unix(article.time_read).fromNow();
     const formattedDateTime = moment.unix(article.time_read).format(dateTimeFormat);
     const displayedTitle = article.resolved_title || article.given_title || article.resolved_url;
+    const formattedTitle = displayedTitle.length > 50 ? displayedTitle.slice(0, 47) + '...' : displayedTitle;
     return (
       <li>
         <a href={article.resolved_url} title={displayedTitle}>
-          {displayedTitle}
+          {formattedTitle} | {articleHost}
         </a>&nbsp;
         <small><abbr title={formattedDateTime}>{relativeDateTime}</abbr></small>
       </li>
